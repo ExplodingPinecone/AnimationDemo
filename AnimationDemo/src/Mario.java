7f2c87e368e1003f7a5a2ded05b1d8b739adc7fd
brandle716 <brandle716@PR28.hhs.win.fuhsd.org>
2018-05-02 14:01:27 -0700

    Added velocity

--- AnimationDemo/src/Mario.java
+++ AnimationDemo/src/Mario.java
@@ -9,20 +9,28 @@ public class Mario extends Sprite {
 
 	public static final int MARIO_WIDTH = 40;
 	public static final int MARIO_HEIGHT = 60;
+	
+	private double xV, yV;
+	private int maxXV, maxYV;
 
 	public Mario(PImage img, int x, int y) {
 		super(img, x, y, MARIO_WIDTH, MARIO_HEIGHT);
+		xV = 0;
+		yV = 0;
+		maxXV = 10;
+		maxYV = 10;
 	}
 
 	// METHODS
 	public void walk(int dir) {
 		// WALK!
-		x += dir;
+		xV += dir;
+		
 	}
 
 	public void jump() {
 		// JUMP!
-		y -= 5;
+		yV -= 2;
 	}
 
 	public void act(ArrayList<Shape> obstacles) {
@@ -35,7 +43,28 @@ public class Mario extends Sprite {
 			}
 		}
 		if(!onGround)
-			super.y++;
+			yV+=1;
+		else if (yV > 0)
+			yV = 0;
+		
+		
+		if(xV > maxXV) {
+			xV = maxXV;
+		} else if(xV < -maxXV) {
+			xV = -maxXV;
+		}
+		
+		if(yV > maxYV) {
+			yV = maxYV;
+		} else if(yV < -maxYV) {
+			yV = -maxYV;
+		}
+		xV *= 0.9;
+		
+		x += xV;
+		y += yV;
+		
+		
 	}
 
 
